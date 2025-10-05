b7845107605067740c747c6009a6dd83e2ece5c9
vengadesh <smile.suvesana@gmail.com>
2025-10-05 20:59:27 +0530

    added moon

--- next.config.ts
+++ next.config.ts
@@ -64,6 +64,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'www.rmg.co.uk',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
